6ca86101a20ef3bd5a6269695a3333d8b110e62b
Severin Buchser <severin.buchser@gmx.ch>
2020-10-16 14:05:15 +0200

    Add AuthGuard
    The AuthGuard is now implemented and used by the RouterModule. It uses the UserService to check if a user is logged in or not. If not, the guard denies access to the 'user/profile' url and redirects the user to 'user/login'. If the user is logged in, the guard provides access to the above url.

--- frontend/src/app/router/router.ts
+++ frontend/src/app/router/router.ts
@@ -1,6 +1,6 @@
 // Components
 import { LoginComponent } from './../components/user/login/login.component';
-import {  RegisterComponent } from './../components/user/register/register.component';
+import { RegisterComponent } from './../components/user/register/register.component';
 import { ProfileComponent } from './../components/user/profile/profile.component';
 import { MyProductsComponent } from './../components/user/profile/my-products/my-products.component';
 import { UserDetailsComponent } from './../components/user/profile/user-details/user-details.component';
@@ -10,6 +10,8 @@ import { BuyProductComponent } from './../components/home/product/buy-product/bu
 import { ProductInformationComponent } from './../components/home/product/product-information/product-information.component';
 import { ApproveProductsComponent } from './../components/user/profile/approve-products/approve-products.component';
 
+// Guards
+import { AuthGuard } from './guards/auth-guard/auth.guard';
 import { defaultUserNavigationElements, defaultProfileComponent } from './../components/user/profile/navigation-elements';
 
 export const routes = [
@@ -33,6 +35,7 @@ export const routes = [
     path: 'user/profile',
     component: ProfileComponent,
     data: defaultUserNavigationElements[defaultProfileComponent],
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'details',
