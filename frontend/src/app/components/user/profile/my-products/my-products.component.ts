09a39e6e701bf0aa11e0aa05a672e08dffdc37d9
Me Do <wesmuesssi@mailbox.org>
2020-10-16 19:16:43 +0200

    add products on my products

--- frontend/src/app/components/user/profile/my-products/my-products.component.ts
+++ frontend/src/app/components/user/profile/my-products/my-products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes } from '@angular/router';
+import { ProductInformationComponent } from 'src/app/components/home/product/product-information/product-information.component';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-my-products',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-products.component.scss']
 })
 export class MyProductsComponent implements OnInit {
+  products: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
+    // for routes with parameters
+    const routes: Routes = [
+      { path: 'user/profile/myproducts/:id' , component: ProductInformationComponent}
+     ];
   }
 
+  ngOnInit(): void {
+    this.productService.getAll().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
