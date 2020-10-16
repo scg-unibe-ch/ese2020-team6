7f73fa26333d598ab94e539d5226058d107238ae
Severin Buchser <severin.buchser@gmx.ch>
2020-10-16 21:02:44 +0200

    Add basic listing of all products in approve products page
    The approve products page uses the ProductCardListComponent to display all the products availavle at the moment. The backend logic is missing for further filtering.

--- frontend/src/app/components/user/profile/approve-products/approve-products.component.ts
+++ frontend/src/app/components/user/profile/approve-products/approve-products.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+// Packages
+import { Component } from '@angular/core';
+// Services
+import { ProductService } from '../../../../services/product/product.service';
+// Models
+import { ProductModel } from '../../../../models/product/product.model';
 
 @Component({
   selector: 'app-approve-products',
   templateUrl: './approve-products.component.html',
   styleUrls: ['./approve-products.component.scss']
 })
-export class ApproveProductsComponent implements OnInit {
+export class ApproveProductsComponent {
 
-  constructor() { }
+  public products: Array<ProductModel>;
 
-  ngOnInit(): void {
+  constructor(
+    private productService: ProductService
+  ) {
+    this.productService.getAll().subscribe((products: Array<ProductModel>) => this.products = products);
   }
-
 }
