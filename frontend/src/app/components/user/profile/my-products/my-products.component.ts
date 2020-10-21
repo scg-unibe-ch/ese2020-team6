a43e4915fb484f65be763d273bb3b4eb590e19d8
Severin Buchser <severin.buchser@gmx.ch>
2020-10-21 23:12:10 +0200

    Change my-products to use product-view component

--- frontend/src/app/components/user/profile/my-products/my-products.component.ts
+++ frontend/src/app/components/user/profile/my-products/my-products.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Routes } from '@angular/router';
 import { ProductInformationComponent } from 'src/app/components/home/product/product-information/product-information.component';
 import { ProfileNavigationElementModel } from 'src/app/models/form/profile-navigation-element.model';
 import { UserModel } from 'src/app/models/user/user.model';
+import { ProductModel } from 'src/app/models/product/product.model';
 import { ProductService } from 'src/app/services/product/product.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { adminNavigationElements, defaultUserNavigationElements } from '../navigation-elements';
@@ -13,19 +14,15 @@ import { adminNavigationElements, defaultUserNavigationElements } from '../navig
   styleUrls: ['./my-products.component.scss']
 })
 export class MyProductsComponent implements OnInit {
-  products: any;
+  public products: Array<ProductModel>;
   public currentContent: ProfileNavigationElementModel;
   public userId: number;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private userService: UserService) {
-    // for routes with parameters
-    const routes: Routes = [
-      { path: 'user/profile/myproducts/:id' , component: ProductInformationComponent}
-     ];
-    }
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(
