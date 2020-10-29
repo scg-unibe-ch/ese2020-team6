07b8e48d47d9ddd955739874d736b64a58e86855
Kirushanth Sivanandan <kirushanth.sivanandan@students.unibe.ch>
2020-10-29 18:47:53 +0100

    Fixed/Updated Validation

--- frontend/src/app/components/custom-form/validators/regex/regex-validator-base.ts
+++ frontend/src/app/components/custom-form/validators/regex/regex-validator-base.ts
@@ -33,12 +33,12 @@ export class RegexValidatorBase implements Validator {
 
 export const validatorRegex = {
   noun: {
-    regex: /^[A-Z][a-z]*$/,
+    regex: /^[A-ZÄÖÜ][a-z_äÄöÖüÜ]*$/,
     name: "nounValidator",
     errorMessage: "Start with a capital Letter!"
   },
   username: {
-    regex: /^[A-Za-z\d@$!%*#?&]{5,255}$/,
+    regex: /^[A-Za-z_äÄöÖüÜ\d@$!%*#?&]{5,255}$/,
     name: "usernameValidator",
     errorMessage: "Min. of 5 characters including numbers and special characters: @$!%*#?&"
   },
@@ -53,34 +53,34 @@ export const validatorRegex = {
     errorMessage: "Inbetween three and ten numbers!"
   },
   houseNumber: {
-    regex: /^[A-Za-z\d]{1,}$/,
+    regex: /^[A-Za-z_äÄöÖüÜ\d]{1,}$/,
     name: "houseNumberValidator",
     errorMessage: "At least one character!"
   },
   email: {
-    regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z_äÄöÖüÜ\-0-9]+\.)+[a-zA-Z_äÄöÖüÜ]{2,}))$/,
     name: "emailValidator",
     errorMessage: "Not a valid Email-Address!"
   },
   password: {
-    regex: /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{7,}$/,
+    regex: /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[@$!%*#?&])[A-Za-z_äÄöÖüÜ\d@$!%*#?&]{7,}$/,
     name: "passwordValidator",
     errorMessage: "Min. 7 characters, at least 1 capital letter, 1 lowercase letter, 1 number and 1 special character: @$!%*#?&"
   },
   usernameOrEmail: {
-    regex: /^((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))|[A-Za-z\d@$!%*#?&]{5,})$/,
+    regex: /^((([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z_äÄöÖüÜ\-0-9]+\.)+[a-zA-Z_äÄöÖüÜ]{2,}))|[A-Za-z_äÄöÖüÜ\d@$!%*#?&]{5,})$/,
     name: "usernameOrEmailValidator",
     errorMessage: "You either need to enter your username or your email address."
   },
   title: {
-    regex: /^[A-Z]([A-Za-z\d@$!%*#?&, ]){4,30}$/,
+    regex: /^(.|\s){5,}$/,
     name: "titleValidatdor",
-    errorMessage: "Max. length of 30 and min. length of 5 and characters. You can use special characters: '@$!%*#?&'"
+    errorMessage: "Min. length of 5 and characters."
   },
   description: {
-    regex: /^[A-Z]([A-Za-z\d@$!%*#?&,.\- ]){50,}$/,
+    regex: /^(.|\s){50,}$/,
     name: "descriptionValidatdor",
-    errorMessage: "Min. length of 50 characters. Has to start with a capital letter. You can use special characters: '@$!%*#?&.,-'"
+    errorMessage: "Min. length of 50 characters."
   },
   price: {
     regex: /^\d*([.]\d{1,})?$/,
@@ -88,9 +88,9 @@ export const validatorRegex = {
     errorMessage: "You can either enter an Integer or a Float."
   },
   location: {
-    regex: /^[A-Za-z\d,\-., ]{1,}$/,
+    regex: /^[A-ZÄÖÜ][A-Za-z_äÄöÖüÜ\d,\-., ]{1,}$/,
     name: "locationValidator",
-    errorMessage: "You can either enter an Integer or a Float."
+    errorMessage: "You can either enter an Integer, Float or limited special characters (-.,). Has to start with a capital letter."
   },
   date: {
     regex: /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|(1|2)[0-9]|3[0-1])T(0[1-9]|1[0-9]|2[0-3]):(0[0-9]|([1-5])[0-9])$/,
@@ -98,7 +98,7 @@ export const validatorRegex = {
     errorMessage: "Date and Time: yyyy-mm-ddThh:minmin"
   },
   rejectionMessage: {
-    regex: /^[A-Z]([A-Za-z\d@$!%*#?&,.\- ]){50,}$/,
+    regex: /^[A-ZÄÖÜ]([A-Za-z_äÄöÖüÜ\d@$!%*#?&,.\- ]){50,}$/,
     name: "rejectionMessageValidator",
     errorMessage: "Min. length of 50 characters. Has to start with a capital letter. You can use special characters: '@$!%*#?&.,-'"
   }
