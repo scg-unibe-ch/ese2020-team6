fff7bbe326705452fa97c854102e0468295d06cf
Me Do <wesmuesssi@mailbox.org>
2020-11-01 06:51:25 +0100

    Implement search feature

--- frontend/src/app/components/home/product/search-product/search-product.component.ts
+++ frontend/src/app/components/home/product/search-product/search-product.component.ts
@@ -1,39 +1,27 @@
-import { SearchService } from './../../../../services/product/search/search.service';
-import { NgForm } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 import { ThemeService } from 'src/app/services/theme/theme.service';
 import { Themable } from 'src/app/models/theme/themable';
-import { ProductService } from 'src/app/services/product/product.service';
 import { SearchModel } from 'src/app/models/request/search/search.model';
-import { ProductModel } from 'src/app/models/product/product.model';
-import { Router } from '@angular/router';
-import { NullPostProductForm, PostProductFormModel } from 'src/app/models/form/post-product-form.model';
+
 
 @Component({
   selector: 'app-search-product',
   templateUrl: './search-product.component.html',
   styleUrls: ['./search-product.component.scss']
 })
-export class SearchProductComponent extends Themable implements OnInit  {
-  criterias: SearchModel;
+export class SearchProductComponent extends Themable  {
+  @Output() criteriaChange = new EventEmitter<SearchModel>();
   products: any;
-  public values = new SearchModel();
+  search = false;
+  public criteria = new SearchModel();
 
   constructor(
-    themeService: ThemeService,
-    private productService: ProductService,
+    themeService: ThemeService
   ) {
     super(themeService);
    }
 
-  ngOnInit(): void {
-  }
-
   onSubmit(): void {
-    this.criterias = this.values;
-    console.log(this.criterias, 'Halloooooooo criterias');
-    console.log(this.values, 'Halloooooooo form');
-    this.productService.getSearchedProducts(this.criterias).subscribe(
-      (values: Array<ProductModel>) => this.products = values);
+    this.criteriaChange.emit(this.criteria);
   }
 }
