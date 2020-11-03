af8db4e2a4567dcbc38b031841f54b1fbf0dc205
Severin Buchser <severin.buchser@gmx.ch>
2020-11-03 02:20:51 +0100

    Get seller on checkout component
    The checkout component now retreives the seller of the product. The seller gets stored in the seller filed, which is a CutUserModel.

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product/product.service';
+import { UserService } from '../../services/user/user.service';
 import { CheckoutRouteParametersModel } from '../../models/checkout/checkout-route-parameters.model';
 import { ProductModel, NullProduct } from '../../models/product/product.model';
+import { CutUserModel, NullCutUser } from '../../models/user/cut-user.model';
 
 
 @Component({
@@ -13,17 +15,25 @@ import { ProductModel, NullProduct } from '../../models/product/product.model';
 export class CheckoutComponent implements OnInit {
 
   private product: ProductModel = new NullProduct();
+  private seller: CutUserModel = new NullCutUser();
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((parameters: CheckoutRouteParametersModel) => {
       let productId: number = parameters.productId;
       this.productService.getProductById(productId)
-      .subscribe((product: ProductModel) => this.product = product);
+      .subscribe((product: ProductModel) => {
+        this.product = product;
+        let sellerId: number = product.userId;
+        this.userService.getUserById(sellerId).subscribe((seller: CutUserModel) => {
+          this.seller = seller;
+        })
+      });
     });
   }
 
