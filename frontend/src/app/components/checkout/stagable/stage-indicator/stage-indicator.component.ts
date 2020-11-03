e9c65712687a876994b514267168c86a81042079
Severin Buchser <severin.buchser@gmx.ch>
2020-11-03 21:42:14 +0100

    Implement stage indicator
    The stage indicator now works as intended: If a stage is cleared and the next stage is active, the indicator shows this by changeing color. The indicator consists out of multiple stops (indicators for stages) and lines, which connect the stops.

--- frontend/src/app/components/checkout/stagable/stage-indicator/stage-indicator.component.ts
+++ frontend/src/app/components/checkout/stagable/stage-indicator/stage-indicator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { StageModel } from '../../../../models/checkout/stage/stage.model'
+import { StageModel } from '../../../../models/checkout/stage/stage.model';
+import { ThemeService } from '../../../../services/theme/theme.service';
+import { Themable } from '../../../../models/theme/themable';
 
 
 @Component({
@@ -7,7 +9,7 @@ import { StageModel } from '../../../../models/checkout/stage/stage.model'
   templateUrl: './stage-indicator.component.html',
   styleUrls: ['./stage-indicator.component.scss']
 })
-export class StageIndicatorComponent implements OnInit {
+export class StageIndicatorComponent extends Themable implements OnInit {
 
   @Input()
   stages: Array<StageModel> = new Array<StageModel>();
@@ -15,9 +17,45 @@ export class StageIndicatorComponent implements OnInit {
   @Input()
   currentStageIndex: number;
 
-  constructor() { }
+  constructor(
+    themeService: ThemeService
+  ) {
+    super(themeService);
+  }
 
   ngOnInit(): void {
   }
 
+  get stageTitles(): Array<string> {
+    return this.stages.map((stage: StageModel) => stage.title);
+  }
+
+  get stops(): Array<[string, number]> {
+    return this.stages.map((stage: StageModel, index: number) => [stage.title, index]);
+  }
+
+  get connectors(): Array<number> {
+    return this.stages.reduce((result: Array<number>, stage: StageModel, index: number) => {
+      if (index + 1 < this.stages.length) result.push(index);
+      return result;
+    }, []);
+  }
+
+  public getIndexOfStage(stage: StageModel): number {
+    return this.stages.indexOf(stage);
+  }
+
+  public getCircleX(stage: StageModel): number {
+    return 50 + 80 * (this.getIndexOfStage(stage));
+  }
+
+  public isVisited(index: number): string {
+    if (index < this.currentStageIndex) return 'visited';
+    else return 'default';
+  }
+
+  public isActive(index: number): string {
+    if (index == this.currentStageIndex) return 'active';
+    else return null;
+  }
 }
