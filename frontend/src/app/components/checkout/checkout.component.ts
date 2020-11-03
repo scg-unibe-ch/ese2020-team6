395154a6e62cd6c828c86d4d18b717c4b24f193d
Severin Buchser <severin.buchser@gmx.ch>
2020-11-03 02:16:35 +0100

    Get product on checkout component
    The checkout component now has a field product. The product gets retreived useing the productId passed on by the router.

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product/product.service';
+import { CheckoutRouteParametersModel } from '../../models/checkout/checkout-route-parameters.model';
+import { ProductModel, NullProduct } from '../../models/product/product.model';
 
 
 @Component({
@@ -10,13 +12,19 @@ import { ProductService } from '../../services/product/product.service';
 })
 export class CheckoutComponent implements OnInit {
 
+  private product: ProductModel = new NullProduct();
+
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService
   ) { }
 
   ngOnInit(): void {
-
+    this.route.params.subscribe((parameters: CheckoutRouteParametersModel) => {
+      let productId: number = parameters.productId;
+      this.productService.getProductById(productId)
+      .subscribe((product: ProductModel) => this.product = product);
+    });
   }
 
 }
