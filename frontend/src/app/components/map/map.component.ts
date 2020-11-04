0618cf6082601bc0b04045a308fb45b0de90f16d
Severin Buchser <severin.buchser@gmx.ch>
2020-11-04 23:13:38 +0100

    Add geosearch with esri-leaflet plugin esri-leaflet-geocoder
    The map component has an Input isSearch. If true, a user can search for geolocations on the map. Else not.

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -1,21 +1,29 @@
-import { OnInit, AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
-import * as L from 'leaflet';
-import { icon, Marker } from 'leaflet';
+import { OnInit, AfterViewInit, OnChanges, Input, Component, ViewChild, ElementRef } from '@angular/core';
+import * as Esri from 'esri-leaflet';
+import * as Geocoder from 'esri-leaflet-geocoder';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit, AfterViewInit {
+export class MapComponent implements OnInit, AfterViewInit, OnChanges {
   @ViewChild('map')
   private mapContainer: ElementRef;
+
+  private map: Leaflet.Map;
+  private search;
+  private results;
+
+  @Input()
+  isSearch: boolean = false;
 
   constructor() {
     const iconRetinaUrl = 'assets/marker-icon-2x.png';
     const iconUrl = 'assets/marker-icon.png';
     const shadowUrl = 'assets/marker-shadow.png';
-    const iconDefault = icon({
+    const iconDefault = Leaflet.icon({
       iconRetinaUrl,
       iconUrl,
       shadowUrl,
@@ -25,7 +33,7 @@ export class MapComponent implements OnInit, AfterViewInit {
       tooltipAnchor: [16, -28],
       shadowSize: [41, 41]
     });
-    Marker.prototype.options.icon = iconDefault;
+    Leaflet.Marker.prototype.options.icon = iconDefault;
   }
 
   public ngOnInit(): void {
@@ -38,23 +46,64 @@ export class MapComponent implements OnInit, AfterViewInit {
 
 
   private initMap(): void {
-    const initialState = {
-      lng: 11,
-      lat: 49,
-      zoom: 4
-    };
-    const map = L.map(this.mapContainer.nativeElement).setView(
-      [initialState.lat, initialState.lng],
-      initialState.zoom
-    );
+    if (!this.map) this.createMap().addBaseLayer();
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    if (this.isSearch) this.addSearch().addResults();
+    else this.removeSearch().removeResults();
+  }
 
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+  public ngOnChanges(): void {
+
+  }
+
+  private createMap(): MapComponent {
+    this.map = Leaflet.map(this.mapContainer.nativeElement).setView([47.096790, 7.598464], 12);
+    return this;
+  }
+
+  private addBaseLayer(): MapComponent {
+    Esri.basemapLayer('Topographic').addTo(this.map);
+    return this;
+  }
+
+  private addSearch(): MapComponent {
+    var arcgisOnline = Geocoder.arcgisOnlineProvider();
+    var gisDay = Geocoder.featureLayerProvider({
+      url: 'https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/GIS_Day_Final/FeatureServer/0',
+      searchFields: ['Name', 'Organization'], // Search these fields for text matches
+      label: 'GIS Day Events', // Group suggestions under this header
+      formatSuggestion: function (feature) {
+        return feature.properties.Name + ' - ' + feature.properties.Organization; // format suggestions like this.
+      }
+    });
+
+    this.search = Geocoder.geosearch({
+      providers: [arcgisOnline, gisDay] // will geocode via ArcGIS Online and search the GIS Day feature service.
+    }).addTo(this.map)
+
+    return this;
+  }
+
+  private removeSearch(): MapComponent {
+    if (this.search) this.map.removeControl(this.search);
+    return this;
+  }
+
+  private addResults(): MapComponent {
+    this.results = Leaflet.layerGroup().addTo(this.map);
+    this.search.on("results", (data) => {
+      this.results.clearLayers();
+      data.results.forEach((result) => {
+        this.results.addLayer(Leaflet.marker(result.latlng));
+      });
+    });
+
+    return this;
+  }
+
+  private removeResults(): MapComponent {
+    if (this.results) this.map.removeLayer(this.results);
+    return this;
   }
 
 }
