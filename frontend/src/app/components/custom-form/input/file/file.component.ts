8153aada047f2b400e749c1ab2cf4ac7964bb242
Kirushanth Sivanandan <kirushanth.sivanandan@students.unibe.ch>
2020-11-04 13:09:12 +0100

    Revert "Revert "Merge branch 'master' into 3-2.1.1-SelectCategories""
    This reverts commit a9f174a1728379d5c1237c0261bf2ad727fbb0c2.

---
+++ frontend/src/app/components/custom-form/input/file/file.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Optional, Inject, ViewChild } from '@angular/core';
+import { NgModel, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ValueAccessorBase } from '../../value-accessor-base';
+import { ThemeService } from '../../../../services/theme/theme.service';
+
+@Component({
+  selector: 'app-file',
+  templateUrl: './file.component.html',
+  styleUrls: ['./file.component.scss'],
+  providers: [
+    {provide: NG_VALUE_ACCESSOR, useExisting: FileComponent, multi: true}
+  ]
+})
+export class FileComponent  extends ValueAccessorBase<String> {
+  public type: string = "file";
+
+  @Input()
+  public showError: boolean = true;
+
+  @Input()
+  public placeholder: string;
+
+  @Input()
+  public accept: string;
+
+  @ViewChild(NgModel)
+  model: NgModel;
+
+  constructor(
+    themeService: ThemeService
+  ) {
+    super(themeService);
+  }
+}
---
+++ frontend/src/app/components/custom-form/validators/date/date-validator.directive.ts
@@ -0,0 +1,35 @@
+import { Directive } from '@angular/core';
+import { NG_VALIDATORS, AbstractControl, ValidationErrors  } from '@angular/forms';
+import { RegexValidatorBase , validatorRegex } from '../regex/regex-validator-base';
+
+@Directive({
+  selector: '[appDateValidator]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: DateValidatorDirective,
+    multi: true
+  }]
+})
+export class DateValidatorDirective extends RegexValidatorBase {
+  constructor() {
+    super(validatorRegex.date);
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    let regexResults: ValidationErrors | null = super.validate(control);
+    if(!regexResults) {
+      let parsedDate: Date = new Date(control.value);
+      let currentDate: Date = new Date();
+      currentDate.setSeconds(0);
+      currentDate.setMilliseconds(0);
+      var difference = parsedDate.getTime() - currentDate.getTime();
+      if (difference <= 0) {
+        return {
+          date: {
+          errorName: "ErrorName",
+          errorMessages: ['Please Choose a date in the future.']
+        }}
+      } else return null;
+    } else return regexResults;
+  }
+}
