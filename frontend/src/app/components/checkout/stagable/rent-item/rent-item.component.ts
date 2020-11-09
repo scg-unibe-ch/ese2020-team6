f9e1ecb2f45e223d3dcd83790433c2f66d18004e
Severin Buchser <severin.buchser@gmx.ch>
2020-11-09 19:26:12 +0100

    Add orderService to RentItemComponent
    The RentItemComponent is now able to request an order to rent an item

--- frontend/src/app/components/checkout/stagable/rent-item/rent-item.component.ts
+++ frontend/src/app/components/checkout/stagable/rent-item/rent-item.component.ts
@@ -2,23 +2,29 @@ import { Component, ComponentFactoryResolver } from '@angular/core';
 import { ShippingComponent } from '../stage/shipping/shipping.component';
 import { DurationComponent } from '../stage/duration/duration.component';
 import { PaymentMethodComponent } from '../stage/payment-method/payment-method.component';
-import { StagableExtention } from '../stagable-extention';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../../../services/product/product.service';
 import { UserService } from '../../../../services/user/user.service';
+import { OrderService } from '../../../../services/order/order.service';
+import { OrderRequestBuilder } from '../../../../models/request/order/order-request-builder.module';
+import { ShippingHoursRequestExtension } from '../../../../models/request/order/order-request-model.module';
+import { ShippingHoursResponseExtension } from '../../../../models/response/order/order-response-model.module';
 
 @Component({
   selector: 'rent-item',
   templateUrl: '../stagable.component.html',
   styleUrls: ['../stagable.component.scss']
 })
-export class RentItemComponent extends StagableExtention {
+export class RentItemComponent extends OrderRequestBuilder<ShippingHoursRequestExtension, ShippingHoursResponseExtension> {
+
+  protected _endpointURLExtention: string = 'order/item/rent';
 
   constructor(
     componentFactoryResolver: ComponentFactoryResolver,
     route: ActivatedRoute,
     productService: ProductService,
-    userService: UserService
+    userService: UserService,
+    orderService: OrderService
   ) {
     super(
       componentFactoryResolver,
@@ -41,11 +47,17 @@ export class RentItemComponent extends StagableExtention {
       ],
       route,
       productService,
-      userService
+      userService,
+      orderService
     );
   }
 
-  protected finalize: (stageIndex: number, data?: any) => void = (stageIndex: number): void => {
-    console.log(this.dataStorage);
+  protected buildOrderRequest(): ShippingHoursRequestExtension {
+    return {
+      productId: this.product.productId,
+      paymentMethod: this.getDataValueByStageIndex(2),
+      shippingAddress: this.getDataValueByStageIndex(0),
+      hours: this.getDataValueByStageIndex(1)
+    }
   }
 }
