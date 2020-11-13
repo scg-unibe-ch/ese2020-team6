8a917c17fe39e8399a8c3bd8abd3b056ea8f9eac
michael.duer <62062370+git-md@users.noreply.github.com>
2020-11-13 16:00:41 +0100

    add rent item and rent service
    -add methods to rent an item or service
    -rename database table names
    -add routing in order.controller

---
+++ backend/src/models/servicesrent.model.ts
@@ -0,0 +1,39 @@
+import { Optional, Model, Sequelize, DataTypes } from 'sequelize';
+
+export interface ServicesRentAttributes {
+    orderId: number;
+    hours: number;
+}
+
+export interface ServicesRent extends Optional<ServicesRentAttributes, 'orderId'> {
+
+}
+
+export class ServicesRent extends Model<ServicesRentAttributes, ServicesRent>
+    implements ServicesRentAttributes {
+        orderId!: number;
+        hours!: number;
+
+        public static initialize(sequelize: Sequelize){
+            ServicesRent.init({
+                orderId:{
+                    type: DataTypes.NUMBER,
+                    primaryKey: true,
+                    allowNull: false,
+                    unique: true
+                },
+                
+                hours: {
+                    type: DataTypes.NUMBER,
+                    allowNull: false
+                },
+               
+            },
+                {
+                    sequelize,
+                    tableName: 'buyService'
+                }
+            );
+        }
+
+    }
--- backend/src/services/order.service.ts
+++ backend/src/services/order.service.ts
@@ -1,6 +1,7 @@
 import { Products, ProductsAttributes } from '../models/products.model';
 import { UserAttributes, User } from '../models/user.model';
 import {Order, Orders} from '../models/order.model';
+import { ServicesRent } from '../models/servicesrent.model';
 
 const {OP} = require('sequelize');
 
@@ -34,6 +35,71 @@ export class OrderService {
                 productId: productId
             }
         });
+        } else {
+            console.log('Error Saldo zu klein');
+        }
+       // return orderId;
+
+
+       
+    }
+
+    public async rentItem(productId: number, hours: number, paymentMethod: string, shipping: string, buyerId: number) {
+        const product = await Products.findOne({
+            where: {
+                productId: productId
+            }
+        });
+        const sellerId: number = product.userId;
+
+        const user = await User.findOne({
+            where: {
+                userId: buyerId
+            }
+        });
+
+        if (user.wallet >= product.price * hours) {
+        // subtract from buyer
+        User.increment( 'wallet', {by: -(product.price * hours), where: { userId: 'buyerId'}});
+        // add to seller
+        User.increment( 'wallet', {by: (product.price * hours), where: { userId: 'sellerId'}});
+      //  Order.build({productId: productId, userId: buyerId, sellerId: user.userId})
+        Products.update({status: 'rent'}, {
+            where: {
+                productId: productId
+            }
+        });
+        } else {
+            console.log('Error Saldo zu klein');
+        }
+       // return orderId;
+    }
+
+    public async purchaseService(serviceId: number, hours: number, paymentMethod: string, shipping: string, buyerId: number) {
+        const service = await Products.findOne({
+            where: {
+                serviceId: serviceId
+            }
+        });
+        const sellerId: number = service.userId;
+
+        const user = await User.findOne({
+            where: {
+                userId: buyerId
+            }
+        });
+
+        if (user.wallet >= service.price * hours) {
+            // subtract from buyer
+            User.increment( 'wallet', {by: -(service.price * hours), where: { userId: 'buyerId'}});
+            // add to seller
+            User.increment( 'wallet', {by: (service.price * hours), where: { userId: 'sellerId'}});
+            //  Order.build({productId: productId, userId: buyerId, sellerId: user.userId})
+            Products.update({status: 'rent'}, {
+                where: {
+                    serviceId: serviceId
+                }
+            });
         } else {
             console.log('Error Saldo zu klein');
         }
