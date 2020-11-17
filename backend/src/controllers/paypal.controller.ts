d2e41b98195480fb91bda0a689300d34c8f6187d
michael.duer <62062370+git-md@users.noreply.github.com>
2020-11-17 13:26:07 +0100

    add more backend logic and add button in frontend
    -add logic to send a json object with the requirement payment information
    -add button in payment/method.component.html to pay with paypal

--- backend/src/controllers/paypal.controller.ts
+++ backend/src/controllers/paypal.controller.ts
@@ -1,8 +1,12 @@
+import { ProductsAttributes } from '../models/products.model';
+
+const PORT = process.env.PORT || 3000;
+
 const express = require('express');
 const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
-  'mode': 'sandbox', //sandbox or live
+  'mode': 'sandbox', // sandbox or live
   'client_id': 'AUqpFgB4xKW02ZNqKi6i-aGL-AfY63s60Aa7yf5PG__ZBYTTnp6lCYB_gPb743Gj97gne2kxwEi14abu',
   'client_secret': 'EM67D3UA3WBcpmiiJSUbbZK1bxkKgqBTEAr-KnJEDv3GOul3OnnB5lu4_vQ1aXu-uxJE3p0dlecJdxau'
 });
@@ -10,7 +14,48 @@ paypal.configure({
 
 const app = express();
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
+app.post('/pay', (req, res) => {
+    const create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Red Sox Hat",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "CHF",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "CHF",
+              "total": "25.00"
+          },
+          "description": "Test description"
+      }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0;i < payment.links.length;i++){
+          if(payment.links[i].rel === 'approval_url'){
+            res.redirect(payment.links[i].href);
+          }
+        }
+    }
+  });
+  
+  });
 
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
