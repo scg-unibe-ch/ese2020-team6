8af345a48adcdfdd29f8da6138714e2fca96ca6c
michael.duer <62062370+git-md@users.noreply.github.com>
2020-11-17 13:36:34 +0100

    fix problem with packages and code layout

--- backend/src/controllers/paypal.controller.ts
+++ backend/src/controllers/paypal.controller.ts
@@ -18,44 +18,44 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.post('/pay', (req, res) => {
     const create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
+      'intent': 'sale',
+      'payer': {
+          'payment_method': 'paypal'
       },
-      "redirect_urls": {
-          "return_url": "http://localhost:3000/success",
-          "cancel_url": "http://localhost:3000/cancel"
+      'redirect_urls': {
+          'return_url': 'http://localhost:3000/success',
+          'cancel_url': 'http://localhost:3000/cancel'
       },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": "Red Sox Hat",
-                  "sku": "001",
-                  "price": "25.00",
-                  "currency": "CHF",
-                  "quantity": 1
+      'transactions': [{
+          'item_list': {
+              'items': [{
+                  'name': 'Red Sox Hat',
+                  'sku': '001',
+                  'price': '25.00',
+                  'currency': 'CHF',
+                  'quantity': 1
               }]
           },
-          "amount": {
-              "currency": "CHF",
-              "total": "25.00"
+          'amount': {
+              'currency': 'CHF',
+              'total': '25.00'
           },
-          "description": "Test description"
+          'description': 'Test description'
       }]
   };
-  
+
   paypal.payment.create(create_payment_json, function (error, payment) {
     if (error) {
         throw error;
     } else {
-        for(let i = 0;i < payment.links.length;i++){
-          if(payment.links[i].rel === 'approval_url'){
+        for (let i = 0; i < payment.links.length; i++) {
+          if (payment.links[i].rel === 'approval_url') {
             res.redirect(payment.links[i].href);
           }
         }
     }
   });
-  
+
   });
 
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
