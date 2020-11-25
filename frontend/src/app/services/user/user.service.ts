f182125abddfa6c5b19aa0c3a50c58cdf3283bd5
Severin Buchser <severin.buchser@gmx.ch>
2020-11-25 15:47:14 +0100

    Fix Bug on UserService

--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { LoginUserService } from './login/login-user.service';
@@ -10,6 +11,7 @@ import { UserModel } from '../../models/user/user.model';
 import { CutUserModel } from '../../models/user/cut-user.model';
 import { PreferenceModel } from '../../models/user/preference/preference.model';
 import { Address } from '../../models/map/address/address.model';
+import { environment } from '../../../environments/environment'
 
 import { OnLoad } from '../on-load';
 
@@ -19,6 +21,7 @@ import { OnLoad } from '../on-load';
 export class UserService extends OnLoad<UserModel> {
 
   constructor(
+    private httpClient: HttpClient,
     private loginUserService: LoginUserService,
     private registerUserService: RegisterUserService,
     private getUserService: GetUserService
@@ -37,8 +40,10 @@ export class UserService extends OnLoad<UserModel> {
     return this.observables.onLoad;
   }
 
-  public register(requestBuilder: any): Observable<RegisterUserResponseModel> {
-    return this.registerUserService.register(requestBuilder);
+  public register(formData: any): Observable<RegisterUserResponseModel> {
+    return this.httpClient.post<RegisterUserResponseModel>(
+      environment.endpointURL + 'user/register', formData);
+    //return this.registerUserService.register(requestBuilder);
   }
 
   protected loadObservable(): Observable<UserModel> {
