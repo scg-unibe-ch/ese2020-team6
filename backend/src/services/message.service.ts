23c189f27c18b3d4485093f30af91f18d060f44c
michael.duer <62062370+git-md@users.noreply.github.com>
2020-12-01 15:45:03 +0100

    implementing several methods in message.service.ts
    -add findOrCreateMessageThread
    -add threadDoesExist
    -add setThreadToAccepted
    -add setToRead
    -add getallMessageThreads (not yet working)

--- backend/src/services/message.service.ts
+++ backend/src/services/message.service.ts
@@ -1,9 +1,10 @@
 //import {MessageService} from "../controllers/messages.controller"
 import {Message, MessageCreationAttributes} from "../models/message.model"
-import {MessageThread, MessageThreadCreationAttributes} from "../models/messageThread.model"
+import {MessageThread, MessageThreadCreationAttributes, MessageThreadAttributes} from "../models/messageThread.model"
 import {MessageThreadParticipants} from "../models/messageThreadParticipants.model"
 
-import { where } from "sequelize/types"
+import { Sequelize, where } from "sequelize/types"
+import { InstanceDoesNotExistError } from '../errors/instance-does-not-exist.error';
 
 export class MessageService{
   
@@ -33,5 +34,72 @@ export class MessageService{
 
     public static createMessageThread(messageThread: MessageThreadCreationAttributes): Promise<MessageThread> {
         return MessageThread.create(messageThread);
+      }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static getMessageThreadsByUserId(userId: number): Promise<Array<MessageThread>>{
+        const test = MessageThreadParticipants.findAll({
+            where: {
+                participantId: userId
+            }
+        })
+        return MessageThread.findAll({
+            attributes: ['messageThreadId', [Sequelize.literal('0'), 'test']
+        ],
+        where : {messageThreadId : test}
+    })
+    /*
+            where: {
+                messageThreadId: test
+            }
+        })
+    }*/
+    }
+    public static getAllMessageThreads(userId: number){
+        return MessageThreadParticipants.findAll({
+            where: {
+                participantId: userId
+            }
+        })
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+      /*
+        Setter helper methods
+      */
+    public static setToRead(messageThreadId: number): Promise<Array<Message>> {
+        Message.update({ readStatus: true}, {
+          where: {
+            messageThreadId: messageThreadId
+          }
+        });
+        return this.getMessagesByThreadId(messageThreadId);
+      }
+
+    public static setThreadToAccepted(messageThreadId: number): Promise<Array<Message>> {
+        MessageThread.update({ isAccepted: true}, {
+          where: {
+            messageThreadId: messageThreadId
+          }
+        });
+        return this.getMessagesByThreadId(messageThreadId);
+      }
+
+    /*
+        Helper methods to check if a thread exists and if not creates one
+    */
+    public static threadDoesExist(messageThread: Partial<MessageThreadAttributes>): Promise<MessageThread> {
+        return MessageThread.findOne({
+          where: messageThread,
+          rejectOnEmpty: new InstanceDoesNotExistError(MessageThread.getTableName())
+        });
+      }
+
+    public static findOrCreateMessageThread(messageThread: MessageThreadCreationAttributes): Promise<MessageThread> {
+        return this.threadDoesExist(messageThread).catch((err: any) => {
+          if (err instanceof InstanceDoesNotExistError) {
+            return MessageThread.create(messageThread);
+          } else {
+            return Promise.reject(err);
+          }
+        });
       }
 }
